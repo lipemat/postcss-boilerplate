50168f8e1dd538a77dc008ba2123af2ed0a94697
lipemat <mat@matlipe.com>
2019-10-24 19:23:39 -0400

    Better description of what postcss min does

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = 'production';
 const runner = require( '../helpers/run-task' );
-// Compile the .css file to .min.css.
+// Compile the /pcss/* files to .min.css
 runner.run( 'postcss:min' );
 runner.run( 'revision' );
