923fe574586fb1e906fd380a17c5ec3e9ffa7d82
lipemat <mat@matlipe.com>
2020-01-24 14:05:50 -0500

    Allow CSS modules to work when compiling non min styles during dist

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,7 +1,23 @@
 process.env.NODE_ENV = 'production';
 const runner = require( '../helpers/run-task' );
-// Compile the /pcss/* files to .css
-runner.run( 'postcss:toCSS' );
-// Compile the /pcss/* files to .min.css
+
+/**
+ * Compile the /pcss/* files to .min.css
+ * The main point of this script is to generate the minified
+ * css file for production.
+ *
+ * @type {string}
+ */
 runner.run( 'postcss:min' );
 runner.run( 'revision' );
+
+/**
+ * Compile the /pcss/* files to .css
+ * Allows for switching to .css on production and not having the
+ * site fall apart.
+ *
+ * @type {string}
+ */
+process.env.NODE_ENV = 'develop';
+delete require.cache[ require.resolve( '../config/postcss' ) ];
+runner.run( 'postcss:toCSS' );
