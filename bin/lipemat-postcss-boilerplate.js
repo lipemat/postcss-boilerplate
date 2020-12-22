996869577623fc02676d0f53b3f415532e2aab9f
lipemat <mat@matlipe.com>
2020-12-22 15:24:41 -0700

    Add fix-pnp script
     * When using PNP loose mode, we get warnings for every module
     * we access which is not strictly declared.
     *
     * There is no built in way in Yarn to disable the warnings.
     * This script modifies the generate .pnp.js file to suppress
     * all loose module warnings unless the environmental variable
     * it set to display all warnings.
     *

--- bin/lipemat-postcss-boilerplate.js
+++ bin/lipemat-postcss-boilerplate.js
@@ -10,15 +10,16 @@ const spawn = require( 'cross-spawn' );
 const args = process.argv.slice( 2 );
 
 const scriptIndex = args.findIndex(
-	x => x === 'start' || x === 'dist' || x === 'test'
+	x => x === 'start' || x === 'dist' || x === 'lint'
 );
 const script = scriptIndex === -1 ? args[ 0 ] : args[ scriptIndex ];
 const nodeArgs = scriptIndex > 0 ? args.slice( 0, scriptIndex ) : [];
 
 switch ( script ) {
 	case 'dist':
-	case 'start':
-	case 'lint': {
+	case 'fix-pnp':
+	case 'lint':
+	case 'start': {
 		const result = spawn.sync(
 			'node',
 			nodeArgs
---
+++ scripts/fix-pnp.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+/**
+ * When using PNP loose mode, we get warnings for every module
+ * we access which is not strictly declared.
+ *
+ * There is no built in way in Yarn to disable the warnings.
+ * This script modifies the generate .pnp.js file to suppress
+ * all loose module warnings unless the environmental variable
+ * it set to display all warnings.
+ *
+ * @usage "scripts": {
+    "postinstall": "lipemat-postcss-boilerplate fix-pnp"
+  },
+ *
+ */
+
+const fs = require( 'fs' );
+const PNP_FILE = './.pnp.js';
+
+fs.readFile( PNP_FILE, 'utf8', ( err, data ) => {
+	if ( err ) {
+		return console.log( err );
+	}
+
+	const result = data.replace( /if \(reference != null\) {/, '// # Warnings suppressed via @lipemat/js-boilerplate/fix-pnp script. \n' +
+		'if (! alwaysWarnOnFallback && reference != null) { \n' +
+					'dependencyReference = reference; \n' +
+		'} else if (alwaysWarnOnFallback && reference != null) {' );
+
+	fs.writeFile( PNP_FILE, result, 'utf8', err => {
+		if ( err ) {
+			return console.log( err );
+		}
+	} );
+} );
+
+console.log( 'The .pnp.js file has been adjusted to no longer display warnings for loose modules.' )
