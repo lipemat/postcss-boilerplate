944c356d78d50efe9b2b5a641cfcec571030286c
lipemat <mat@matlipe.com>
2021-01-05 12:19:12 -0700

    Fix usage of "" for `css_folder`
    If a user passes "" as the `css_folder` it will test as false
    then be replaced. Instead we check for the property and use
    whatever is provide if found.

--- helpers/package-config.js
+++ helpers/package-config.js
@@ -9,7 +9,10 @@ let packageConfig = require( path.resolve( workingDirectory, 'package.json' ) );
 packageConfig.workingDirectory = workingDirectory;
 packageConfig.theme_path = packageConfig.theme_path || "../wp-content/themes/core/";
 packageConfig.root = packageConfig.root || "../";
-packageConfig.css_folder = packageConfig.css_folder || "css/";
+// Could be set to "" which would always test false.
+if ( ! packageConfig.hasOwnProperty( 'css_folder' ) ) {
+	packageConfig.css_folder = "css/";
+}
 packageConfig.file_name = packageConfig.file_name || "front-end";
 packageConfig.regenerate_revision = packageConfig.regenerate_revision || false;
 
