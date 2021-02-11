18d722cfec74a9b97400cae94cd2c4b150295506
lipemat <mat@matlipe.com>
2021-02-10 17:46:58 -0700

    Add `fix-pnp` to scripts list
    Allow it to be found in the `scriptIndex`

--- bin/lipemat-postcss-boilerplate.js
+++ bin/lipemat-postcss-boilerplate.js
@@ -10,7 +10,7 @@ const spawn = require( 'cross-spawn' );
 const args = process.argv.slice( 2 );
 
 const scriptIndex = args.findIndex(
-	x => x === 'start' || x === 'dist' || x === 'lint'
+	x => x === 'start' || x === 'dist' || x === 'lint' || x === 'fix-pnp'
 );
 const script = scriptIndex === -1 ? args[ 0 ] : args[ scriptIndex ];
 const nodeArgs = scriptIndex > 0 ? args.slice( 0, scriptIndex ) : [];
