6f789eaa34565b25986a77db05568e025392649e
lipemat <mat@matlipe.com>
2021-09-16 12:25:17 -0400

    Include the `.pnp.cjs` file in fix-pnp script
    Yarn 3.0.0 uses `.pnp.cjs` instead of `.pnp.js` which still needs to be updated to suppress errors.  We now update both files.

--- scripts/fix-pnp.js
+++ scripts/fix-pnp.js
@@ -16,23 +16,31 @@
  */
 
 const fs = require( 'fs' );
-const PNP_FILE = './.pnp.js';
 
-fs.readFile( PNP_FILE, 'utf8', ( err, data ) => {
-	if ( err ) {
-		return console.log( err );
+const PNP_FILES = [
+	'./.pnp.js',
+	'./.pnp.cjs',
+];
+
+PNP_FILES.forEach( PNP_FILE => {
+	if ( fs.existsSync( PNP_FILE ) ) {
+		fs.readFile( PNP_FILE, 'utf8', ( readError, data ) => {
+			if ( readError ) {
+				return console.log( readError );
+			}
+
+			const result = data.replace( /if \(reference != null\) {/, '// # Warnings suppressed via @lipemat/js-boilerplate/fix-pnp script. \n' +
+				'if (! alwaysWarnOnFallback && reference != null) { \n' +
+				'dependencyReference = reference; \n' +
+				'} else if (alwaysWarnOnFallback && reference != null) {' );
+
+			fs.writeFile( PNP_FILE, result, 'utf8', writeError => {
+				if ( writeError ) {
+					return console.log( writeError );
+				}
+				console.log( `The ${PNP_FILE} file has been adjusted to no longer display warnings for loose modules.` );
+			} );
+		} );
 	}
-
-	const result = data.replace( /if \(reference != null\) {/, '// # Warnings suppressed via @lipemat/postcss-boilerplate/fix-pnp script. \n' +
-		'if (! alwaysWarnOnFallback && reference != null) { \n' +
-					'dependencyReference = reference; \n' +
-		'} else if (alwaysWarnOnFallback && reference != null) {' );
-
-	fs.writeFile( PNP_FILE, result, 'utf8', err => {
-		if ( err ) {
-			return console.log( err );
-		}
-	} );
 } );
 
-console.log( 'The .pnp.js file has been adjusted to no longer display warnings for loose modules.' )
