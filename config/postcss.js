ada7946f3c66cff5c33fccbba4b39444da35c876
migueliriano <miguel.angel@x-team.com>
2022-03-28 20:46:09 -0400

    Use base52 for hash encoding on production of CSS classes
    Base64 encode include numbers, for CSS classes we can't use the number
    at the beggining of a class, for that reason postcss-module include a
    leading `_` from generated CSS classes to avoid error.
    
    To have fewer characters in the final generated classes, we are changed
    the encode for base52, which only include alphanumeric and is not
    include the leading `_`

--- config/postcss.js
+++ config/postcss.js
@@ -24,7 +24,7 @@ const compileOptions = {
 			extension: 'pcss',
 			plugins: [
 				require( 'postcss-modules' )( {
-					generateScopedName: 'production' === process.env.NODE_ENV ? '[contenthash:base64:5]' : 'Ⓜ[name]__[local]__[contenthash:base64:2]',
+					generateScopedName: 'production' === process.env.NODE_ENV ? '[contenthash:base52:5]' : 'Ⓜ[name]__[local]__[contenthash:base64:2]',
 					Loader: FileSystemLoader.default,
 					globalModulePaths: [
 						new RegExp( '.*?' + config.theme_path.replace( /\//g, '\\\\' ) + 'pcss', 'i' ),
