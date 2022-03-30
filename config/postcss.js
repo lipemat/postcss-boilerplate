ae32d9613c9247c40ffd69f7f36f8db0c50c0403
lipemat <mat@matlipe.com>
2022-03-30 00:19:12 -0400

    Revert "Prevent leading `_` character from being generated in CSS class names"
    This reverts commit b36fc530

--- config/postcss.js
+++ config/postcss.js
@@ -1,14 +1,12 @@
 const config = require( '../helpers/package-config' );
 const postcssPresetEnv = require( 'postcss-preset-env' );
 const FileSystemLoader = require( '../lib/FileSystemLoader' );
-const genericNames = require( "generic-names" );
+const modulesFolder = 'production' === process.env.NODE_ENV ? '_css-modules-json/min/' : '_css-modules-json/';
 const fs = require( 'fs' );
 const path = require( 'path' );
 const {getDefaultBrowsersList} = require( '../helpers/config' );
 
-const modulesFolder = 'production' === process.env.NODE_ENV ? '_css-modules-json/min/' : '_css-modules-json/';
 const presetEnv = {};
-
 /**
  * If browserslist is not specified, we fallback to WordPress defaults.
  *
@@ -18,30 +16,6 @@ if ( getDefaultBrowsersList() ) {
 	presetEnv.browsers = getDefaultBrowsersList();
 }
 
-/**
- * PostCSS Modules uses Generic Names as is to generate CSS class
- * names as is. Generic Names prepends an extra `_` in front of
- * any class, which starts with a number.
- * Instead, we follow what CSS Loader for Webpack removes the leading
- * numbers before slicing the result to the correct length.
- * We always get a 5 digit class name with no leading number
- * nor superfluous underscore.
- *
- * @link https://github.com/webpack-contrib/css-loader/blob/71d317e8797972d135e616470e3d9d89abd0d829/src/utils.js#L390
- * @link https://github.com/madyankin/postcss-modules/blob/7d5965d4df201ef301421a5e35805d1b47f3c914/src/index.js#L23
- * @link https://github.com/css-modules/generic-names/blob/71e74af6538425ada4dab5b63019402aaa66f871/index.js#L27
- */
-const getGenericName = genericNames( '[contenthash:base64:10]' );
-const generateScopedName = function ( localName, filepath ) {
-	const fullName = getGenericName( localName, filepath );
-	return fullName
-		// Replace any underscores or dashes.
-		.replace( /[_-]+/g, "" )
-		// Replace all leading digits.
-		.replace( /^\d+/, "" )
-		.slice( 0, 5 );
-};
-
 
 const compileOptions = {
 	map: true,
@@ -50,15 +24,15 @@ const compileOptions = {
 			extension: 'pcss',
 			plugins: [
 				require( 'postcss-modules' )( {
-					generateScopedName: 'production' === process.env.NODE_ENV ? generateScopedName : 'Ⓜ[name]__[local]__[contenthash:base64:2]',
+					generateScopedName: 'production' === process.env.NODE_ENV ? '[contenthash:base64:5]' : 'Ⓜ[name]__[local]__[contenthash:base64:2]',
 					Loader: FileSystemLoader.default,
 					globalModulePaths: [
 						new RegExp( '.*?' + config.theme_path.replace( /\//g, '\\\\' ) + 'pcss', 'i' ),
 						new RegExp( '.*?' + config.theme_path + 'pcss', 'i' ),
 					],
 					/**
-					 * Custom output of CSS modules JSON file to specified location.
-					 * Also excludes json files from the global pcss files.
+					 * Custom output of css modules JSON file to specified location
+					 * Also excludes json files from the global pcss files
 					 */
 					getJSON( cssFileName, json ) {
 						const path = require( 'path' );
@@ -74,6 +48,8 @@ const compileOptions = {
 						/**
 						 * We use the Sync method here to fix issues where JSON is not
 						 * being generated.
+						 *
+						 * @since 2019-01-22
 						 */
 						fse.outputJsonSync( jsonFileName, json );
 					},
