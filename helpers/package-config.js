2d41f2f13d201a74951b49f01039fb3d2f6f1734
lipemat <mat@matlipe.com>
2022-05-12 14:53:03 -0400

    Add `shortCssClasses` to default package-config

--- helpers/package-config.js
+++ helpers/package-config.js
@@ -7,12 +7,13 @@ const workingDirectory = fs.realpathSync( process.cwd() );
 
 let packageConfig = require( path.resolve( workingDirectory, 'package.json' ) );
 packageConfig.workingDirectory = workingDirectory;
-packageConfig.theme_path = packageConfig.theme_path || "./";
+packageConfig.theme_path ||= "./";
 // Could be set to "" which would always test false.
 if ( ! packageConfig.hasOwnProperty( 'css_folder' ) ) {
 	packageConfig.css_folder = "css/";
 }
-packageConfig.file_name = packageConfig.file_name || "front-end";
+packageConfig.file_name ||= "front-end";
+packageConfig.shortCssClasses ||= false;
 
 try {
 	let localConfig = require( path.resolve( workingDirectory, './local-config.json' ) );
