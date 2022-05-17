60d578db0f5325841282a0f46abd3c91d17a432b
lipemat <mat@matlipe.com>
2022-05-16 21:54:40 -0400

    Update lib/postcss-clean to work with PostCSS 8
    Refactor to use the PostCSS 8 API.
    
    Vastly different from the original/linked package.

--- src/postcss-clean.js
+++ src/postcss-clean.js
@@ -3,13 +3,12 @@
 /**
  * Mimic the postcss-clean plugin.
  *
- * Override due to the plugin using version 4 of clean-css which
- * has issues with PostCSS 7 and results in inconstant CSS.
+ * Override due to the plugin using version 4 of clean-css, which
+ * has issues with PostCSS 7/8 and results in inconstant CSS.
  *
  * This may potentially be removed in favor of using that plugin again if
- * they change the version of PostCSS to 7 and Clean CSS to 5.
- * We've already tested using those versions and it works, just need the
- * maintainer to bump the versions.
+ * they change the version of PostCSS to 8 and Clean CSS to 5.
+ *
  * Decided to use this `lib` instead of maintaining another fork.
  *
  * @link https://www.npmjs.com/package/postcss-clean
@@ -18,25 +17,28 @@
 const postcss = require( 'postcss' );
 const CleanCss = require( 'clean-css' );
 
-const initializer = ( opts = {} ) => {
-	const cleancss = new CleanCss( opts );
+module.exports = ( opts = {} ) => {
+	const clean = new CleanCss( opts );
 
-	return ( css, res ) => {
-		return new Promise( ( resolve, reject ) => {
-			cleancss.minify( css.toString(), ( err, min ) => {
-				if ( err ) {
-					return reject( new Error( err.join( '\n' ) ) );
-				}
+	return {
+		postcssPlugin: 'clean',
+		OnceExit( css, {result} ) {
+			return new Promise( ( resolve, reject ) => {
+				clean.minify( css.toString(), ( err, min ) => {
+					if ( err ) {
+						return reject( new Error( err.join( '\n' ) ) );
+					}
 
-				for ( const w of min.warnings ) {
-					res.warn( w );
-				}
+					for ( const w of min.warnings ) {
+						result.warn( w );
+					}
 
-				res.root = postcss.parse( min.styles );
-				resolve();
+					result.root = postcss.parse( min.styles );
+					resolve();
+				} );
 			} );
-		} );
+		},
 	};
 };
 
-module.exports = postcss.plugin( 'clean', initializer );
+module.exports.postcss = true;
