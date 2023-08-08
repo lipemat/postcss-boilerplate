e5106ac67589c8675638a6318f2e18eab9bf7e6f
lipemat <mat@matlipe.com>
2023-08-08 16:26:31 -0500

    Fix eslint errors inbin script

--- bin/lipemat-postcss-boilerplate.js
+++ bin/lipemat-postcss-boilerplate.js
@@ -4,15 +4,15 @@
 // Update notifier.
 const updateNotifier = require( 'update-notifier' );
 const pkg = require( '../package.json' );
-updateNotifier( { pkg } ).notify();
+updateNotifier( {pkg} ).notify();
 
 const spawn = require( 'cross-spawn' );
 const args = process.argv.slice( 2 );
 
 const scriptIndex = args.findIndex(
-	x => x === 'browserslist' || x === 'start' || x === 'dist' || x === 'lint' || x === 'fix-pnp'
+	x => 'browserslist' === x || 'start' === x || 'dist' === x || 'lint' === x || 'fix-pnp' === x
 );
-const script = scriptIndex === -1 ? args[ 0 ] : args[ scriptIndex ];
+const script = -1 === scriptIndex ? args[ 0 ] : args[ scriptIndex ];
 const nodeArgs = scriptIndex > 0 ? args.slice( 0, scriptIndex ) : [];
 
 switch ( script ) {
@@ -26,16 +26,16 @@ switch ( script ) {
 			nodeArgs
 				.concat( require.resolve( '../scripts/' + script ) )
 				.concat( args.slice( scriptIndex + 1 ) ),
-			{ stdio: 'inherit' }
+			{stdio: 'inherit'}
 		);
 		if ( result.signal ) {
-			if ( result.signal === 'SIGKILL' ) {
+			if ( 'SIGKILL' === result.signal ) {
 				console.log(
 					'The build failed because the process exited too early. ' +
 					'This probably means the system ran out of memory or someone called ' +
 					'`kill -9` on the process.'
 				);
-			} else if ( result.signal === 'SIGTERM' ) {
+			} else if ( 'SIGTERM' === result.signal ) {
 				console.log(
 					'The build failed because the process exited too early. ' +
 					'Someone might have called `kill` or `killall`, or the system could ' +
