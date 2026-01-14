b81844eb2f0d1534adf65e79a63663b63034e7ee
lipemat <mat@matlipe.com>
2026-01-14 14:48:21 -0500

    Remove js-boilerplate as a dependency

--- jest/jest.config.ts
+++ jest/jest.config.ts
@@ -1,3 +1,10 @@
-const config = require( '@lipemat/js-boilerplate/config/jest.config' );
-config.testEnvironment = 'node';
-module.exports = config;
+import type {Config} from 'jest';
+import config from '@lipemat/js-boilerplate-shared/config/jest.config.js';
+
+const jestConfig: Config = config;
+
+jestConfig.setupFilesAfterEnv = [
+	'<rootDir>/setup.ts',
+];
+
+export default jestConfig;
