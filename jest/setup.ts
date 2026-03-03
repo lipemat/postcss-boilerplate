fe536ec983390f02fa63fc9499c55689b5b04f0e
lipemat <mat@matlipe.com>
2026-03-03 09:13:55 -0500

    Fix mocking of `getPackageConfig` location
    Must call `getPackageConfig` else non on the defaults will work.
    
    Amazing tests have run this long without this fix.

--- jest/setup.ts
+++ jest/setup.ts
@@ -1,13 +1,13 @@
 process.env.THEME_PATH = ( __dirname + '/theme/' ).replace( /\\/g, '/' );
 
 /**
- * Use our tests "theme" path as theme path for files
+ * Use our tests "theme" path as the theme path for files
  * will be loaded from there when applicable.
  */
 jest.mock( '@lipemat/js-boilerplate-shared/helpers/package-config.js', () => ( {
 	...jest.requireActual( '@lipemat/js-boilerplate-shared/helpers/package-config.js' ),
 	getPackageConfig: () => ( {
-		...jest.requireActual( '@lipemat/js-boilerplate-shared/helpers/package-config.js' ),
+		...jest.requireActual( '@lipemat/js-boilerplate-shared/helpers/package-config.js' ).getPackageConfig(),
 		// Point to our data directory for the theme_path.
 		theme_path: 'jest/theme/',
 	} ),
